4a49cb3b28f729cbbd77bf487850cfdca6a22299
Ryan Anderson <ryan@thalana.co.za>
2022-02-14 14:09:05 +0000

    black

--- spatial/spatialcreate.py
+++ spatial/spatialcreate.py
@@ -8,7 +8,7 @@ from matplotlib import pyplot as plt
 from tqdm import tqdm
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--data-dir', required=True, type=str, default=None)
+parser.add_argument("--data-dir", required=True, type=str, default=None)
 args = parser.parse_args()
 assert (args.data_dir is not None) and (os.path.isdir(args.data_dir))
 
@@ -18,11 +18,11 @@ def load_images(path):
     with gzip.open(path) as bytestream:
         # read meta information
         header_buffer = bytestream.read(16)
-        header = np.frombuffer(header_buffer, dtype='>i4')
+        header = np.frombuffer(header_buffer, dtype=">i4")
         magic, n, x, y = header
         # read data
         buffer = bytestream.read(x * y * n)
-        data = np.frombuffer(buffer, dtype='>u1').astype(np.float32)
+        data = np.frombuffer(buffer, dtype=">u1").astype(np.float32)
         data = data.reshape(n, x * y)
     return data
 
@@ -31,7 +31,7 @@ def load_labels(path):
     with gzip.open(path) as bytestream:
         # read meta information
         header_buffer = bytestream.read(8)
-        header = np.frombuffer(header_buffer, dtype='>i4')
+        header = np.frombuffer(header_buffer, dtype=">i4")
         magic, n = header
         # read data
         buffer = bytestream.read(n)
@@ -40,11 +40,11 @@ def load_labels(path):
 
 
 def load_data():
-    train_images_gz = 'train-images-idx3-ubyte.gz'
-    train_labels_gz = 'train-labels-idx1-ubyte.gz'
+    train_images_gz = "train-images-idx3-ubyte.gz"
+    train_labels_gz = "train-labels-idx1-ubyte.gz"
 
-    test_images_gz = 't10k-images-idx3-ubyte.gz'
-    test_labels_gz = 't10k-labels-idx1-ubyte.gz'
+    test_images_gz = "t10k-images-idx3-ubyte.gz"
+    test_labels_gz = "t10k-labels-idx1-ubyte.gz"
 
     data_dir = args.data_dir
 
@@ -69,8 +69,12 @@ def create_spatial(images, labels, sample_size=50, plot=False):
     grid = np.array([[i, j] for j in range(27, -1, -1) for i in range(28)])
     for i, image in enumerate(tqdm(images)):
         replace = True if (sum(image > 0) < sample_size) else False
-        ix = np.random.choice(range(28*28), size=sample_size,
-                              p=image/sum(image), replace=replace)
+        ix = np.random.choice(
+            range(28 * 28),
+            size=sample_size,
+            p=image / sum(image),
+            replace=replace,
+        )
         spatial[i, :, :] = grid[ix] + np.random.uniform(0, 1, (sample_size, 2))
 
     # sanity check
@@ -84,15 +88,15 @@ def create_spatial(images, labels, sample_size=50, plot=False):
             axs[i].set_yticks([])
             axs[i].set_xlim([0, 27])
             axs[i].set_ylim([0, 27])
-            axs[i].set_aspect('equal', adjustable='box')
+            axs[i].set_aspect("equal", adjustable="box")
         plt.show()
 
-    spatial_path = os.path.join(args.data_dir, 'spatial/spatial.pkl')
-    with open(spatial_path, 'wb') as file:
+    spatial_path = os.path.join(args.data_dir, "spatial/spatial.pkl")
+    with open(spatial_path, "wb") as file:
         pickle.dump(spatial, file)
 
-    labels_path = os.path.join(args.data_dir, 'spatial/labels.pkl')
-    with open(labels_path, 'wb') as file:
+    labels_path = os.path.join(args.data_dir, "spatial/labels.pkl")
+    with open(labels_path, "wb") as file:
         pickle.dump(labels, file)
 
 
@@ -100,6 +104,6 @@ def main():
     images, labels = load_data()
     create_spatial(images, labels, sample_size=50, plot=False)
 
-if __name__ == '__main__':
-    main()
 
+if __name__ == "__main__":
+    main()
